26d36249dc13aa8d3a6e2a780f0ad96ae95206f1
Yaroslav Orlov <orlov.iaa@phystech.edu>
2026-05-06 00:46:49 +0300

    update help

--- internal/cli/help.go
+++ internal/cli/help.go
@@ -2,15 +2,42 @@ package cli
 
 var helpMessage string = `mws profile - CLI for profiles managing
 Usage:
-  mws profile create --name=<name> --user=<user> --project=<project>
-  mws profile get --name=<name>
-  mws profile list
-  mws profile delete --name=<name>
-  mws help
+  mws <command> [arguments]
 
 Commands:
-  profile create   Create a profile
+  profile create   Create a new profile
   profile get      Show profile by name
-  profile list     List all profiles
+  profile list     List profiles in the current directory
   profile delete   Delete profile by name
-  help             Show this help message`
+  help             Show this help message
+
+Profile commands:
+  mws profile create --name=<name> --user=<user> --project=<project> [--force|-f]
+  mws profile get --name=<name>
+  mws profile list [-a|-e|-l]
+  mws profile delete --name=<name>
+  mws profile help
+
+Options:
+  --name       Profile name. Used as the YAML file name without extension
+  --user       User value saved into the profile
+  --project    Project value saved into the profile
+  --force, -f  Overwrite an existing profile without confirmation
+
+List options:
+  -l           Show only valid profiles
+  -e           Show valid profiles and profiles with extra YAML fields
+  -a           Show all YAML profile files, including invalid ones
+
+Examples:
+  mws profile create --name=test --user=example --project=new-project
+  mws profile create --name=test --user=example --project=new-project --force
+  mws profile get --name=test
+  mws profile list
+  mws profile list -l
+  mws profile delete --name=test
+
+Notes:
+  Profiles are stored as YAML files in the current directory.
+  The profile name corresponds to the file name: <name>.yaml.
+  Each profile file contains two string fields: user and project.`
