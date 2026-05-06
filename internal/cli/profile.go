663b17bab7ae4f64a0dd746d5f3e95155cd503ac
Yaroslav Orlov <orlov.iaa@phystech.edu>
2026-05-06 17:33:06 +0300

    change string in askToOverwrite

--- internal/cli/profile.go
+++ internal/cli/profile.go
@@ -208,7 +208,7 @@ func printGetOutput(p profile.Profile) error {
 
 func askToOverwrite(name string) (bool, error) {
 	reader := bufio.NewReader(os.Stdin)
-	_, err := fmt.Printf("Profile %s already exist. Do you really want to overwrite it? [y/[n]]", name)
+	_, err := fmt.Printf("Profile %s already exists. Do you really want to overwrite it? [y/N]", name)
 	if err != nil {
 		return false, err
 	}
