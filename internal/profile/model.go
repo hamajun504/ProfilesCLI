54858643da58c70fabde58016fe74549f896a1f9
Yaroslav Orlov <orlov.iaa@phystech.edu>
2026-05-06 17:03:14 +0300

    couple comments

--- internal/profile/model.go
+++ internal/profile/model.go
@@ -1,10 +1,12 @@
 package profile
 
+// ProfileData describes data stored in a profile YAML file.
 type ProfileData struct {
 	User    string `yaml:"user"`
 	Project string `yaml:"project"`
 }
 
+// Profile describes a named profile with its YAML data.
 type Profile struct {
 	Name string
 	Data ProfileData
